c57b30a994af326bb931e325826cf2300571b815
TonyKioko <tonykioko384@gmail.com>
2018-09-27 18:13:13 +0300

    superuser created

--- pichaza/models.py
+++ pichaza/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+import datetime as dt
+
 
 # Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length = 60)
     pic = models.ImageField(upload_to = 'pichazza/')
     description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
