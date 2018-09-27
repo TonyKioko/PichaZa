2e4ef62f22710879c7a9e9692225684780ff8af1
TonyKioko <tonykioko384@gmail.com>
2018-09-27 18:27:36 +0300

    methods added to IMage class

--- pichaza/models.py
+++ pichaza/models.py
@@ -5,6 +5,20 @@ import datetime as dt
 # Create your models here.
 class Image(models.Model):
     name = models.CharField(max_length = 60)
-    pic = models.ImageField(upload_to = 'pichazza/')
+    pic = models.ImageField(upload_to = 'pichazza/', default='NO IMAGE')
     description = models.TextField()
     upload_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_images(cls):
+        images = Image.objects.all()
+        return images
