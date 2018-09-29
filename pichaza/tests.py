1f8c2d61b9feb330bc867e34fe69f8e79c8d146a
TonyKioko <tonykioko384@gmail.com>
2018-09-29 11:51:00 +0300

    location test added and passing

--- pichaza/tests.py
+++ pichaza/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.location= Location(location = 'Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+    def test_delete_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.location.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
